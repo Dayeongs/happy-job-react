38d56157c39949e6dc4fef7342c6ea5b2b6184d5
Dayeongs <ekdud9636@gmail.com>
2024-07-26 17:20:44 +0900

    #01 공지사항 검색 컴포넌트 등록

--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -1,15 +1,22 @@
-import { RouteObject, createBrowserRouter } from 'react-router-dom';
-import { Login } from '../pages/Login';
-import { DashBoard } from '../component/layout/DashBoard/DashBoard';
-import { NotFound } from '../component/common/NotFound/NotFound';
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Login } from "../pages/Login";
+import { DashBoard } from "../component/layout/DashBoard/DashBoard";
+import { NotFound } from "../component/common/NotFound/NotFound";
+import { Notice } from "../pages/Notice";
 
 const routers: RouteObject[] = [
-    { path: '*', element: <NotFound /> },
-    { path: '/', element: <Login /> },
-    {
-        path: '/react',
-        element: <DashBoard />,
-    },
+  { path: "*", element: <NotFound /> },
+  { path: "/", element: <Login /> },
+  {
+    path: "/react",
+    element: <DashBoard />,
+    children: [
+      {
+        path: "system",
+        children: [{ path: "notice.do", element: <Notice /> }],
+      },
+    ],
+  },
 ];
 
 export const Routers = createBrowserRouter(routers);
